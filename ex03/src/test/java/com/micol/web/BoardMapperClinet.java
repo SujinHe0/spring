9e4c5a4975be5434991c62ef046258d123163d98
sujin <hsujin0819@gamil.com>
2022-12-08 18:36:55 +0900

    1208  commit

---
+++ ex03/src/test/java/com/micol/web/BoardMapperClinet.java
@@ -0,0 +1,28 @@
+package com.micol.web;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.micol.web.board.BoardMapper;
+import com.micol.web.board.BoardVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class BoardMapperClinet {
+	
+	@Autowired BoardMapper mapper;
+	
+	@Test
+	public void insertBrd() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("하이");
+		vo.setContent("덜덜");
+		vo.setWriter("ㅎㅎ");
+		int r =  mapper.insertBrd(vo);
+		System.out.println(r + "건이 등록되었습니다.");
+	}
+}
